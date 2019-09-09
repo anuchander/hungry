99665315cfc61634027a46ad15a8fe7b87d472fd
anuchander <54872530+anuchander@users.noreply.github.com>
2019-09-09 12:05:16 -0700

    update with or command for 'y' and 'Y' options

--- hungry.py
+++ hungry.py
@@ -1,5 +1,7 @@
 hungry=input("are you hungry?")
-if hungry=="yes" and hungry=="y" and hungry=="Y":
+if hungry=="yes" or hungry=="y" or hungry=="Y":
     print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
 else:
     print("do homework")    
