b31da69857d22a831feb10de65ccafee66a1b47e
anuchander <54872530+anuchander@users.noreply.github.com>
2022-05-03 18:14:12 -0700

    created file thirsty.py

---
+++ thirsty.py
@@ -0,0 +1,7 @@
+thirsty=input("Are you thirsty?")
+if thirsty=="Yes" or thirsty=='y' or thirsty=="Y" or thirsty=="yes":
+    print("drink water")
+    print("drink juice")
+    print("drink milk")
+else:
+    print("do homework")
